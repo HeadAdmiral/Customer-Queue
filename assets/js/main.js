c0ebcd81ca78e64f7aa1d6e440b14915d5bbfedc
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-26 09:34:07 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,24 @@
+const queue = document.getElementById("queue");
+const modal = document.getElementById("modal");
+const form = document.getElementById("form");
 
+function showModal() {
+  modal.style.display = "block";
+}
+
+function hideModal() {
+  modal.style.display = "none";
+}
+
+function addCustomer(){
+  let firstname = document.getElementById("firstname");
+  let lastname = document.getElementById("lastname");
+  let customer = document.createElement("div");
+  
+  hideModal();
+  
+  customer.classList.add("customer");
+  customer.innerHTML = firstname + " " + lastname;
+  
+  queue.appendChild(customer);
+}
