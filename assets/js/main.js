18edc80b05d49958394d5d42dedf0bdae47bf3fe
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-28 18:31:53 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,40 +34,28 @@ function addCustomer(){
   console.log("added customer to queue");
 }
 
-// credit where credit's due; http://thecodeplayer.com/walkthrough/ripple-click-effect-google-material-design
-
-var element, circle, d, x, y;
-$("button span").click(function(e){
-	console.log("Clicked a span in a button");
-  
-	element = $(this);
-  
-	if(element.find(".circle").length == 0)
-		element.prepend("<span class='circle'></span>");
-		
-	circle = element.find(".circle");
-	circle.removeClass("animate");
-	
-	if(!circle.height() && !circle.width())
-  {
-		d = Math.max(element.outerWidth(), element.outerHeight());
-		circle.css({height: d, width: d});
-	}
-	
-	x = e.pageX - element.offset().left - circle.width()/2;
-	y = e.pageY - element.offset().top - circle.height()/2;
-	
-	circle.css({top: y+'px', left: x+'px'}).addClass("animate");
-})
-
-
-
-$("span").click(function(e){
-	console.log("Clicked a span.");
-})
-
 $( document ).ready(function() {
-    $("button").click(function(e){
-      console.log("Clicked a button.");
+    var element, circle, d, x, y;
+    $("button span").click(function(e){
+      console.log("Clicked a span in a button");
+
+      element = $(this);
+
+      if(element.find(".circle").length == 0)
+        element.prepend("<span class='circle'></span>");
+
+      circle = element.find(".circle");
+      circle.removeClass("animate");
+
+      if(!circle.height() && !circle.width())
+      {
+        d = Math.max(element.outerWidth(), element.outerHeight());
+        circle.css({height: d, width: d});
+      }
+
+      x = e.pageX - element.offset().left - circle.width()/2;
+      y = e.pageY - element.offset().top - circle.height()/2;
+
+      circle.css({top: y+'px', left: x+'px'}).addClass("animate");
     })
 });
