1b44da1ddb9ecb253cb85cdd5727bbf73e899bcd
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-08-28 20:05:01 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,33 +34,87 @@ function addCustomer(){
   console.log("added customer to queue");
 }
 
-$( document ).ready(function() {
-    var parent, ink, d, x, y;
-    $("button").click(function(e){
-      parent = $(this).parent();
-      self = $(this);
-      //create .ink element if it doesn't exist
-      if(self.find(".ink").length == 0)
-        self.prepend("<span class='ink'></span>");
+$(document).ready((function($, undefined) {
+  $("html").addClass("ink-btn");
 
-      ink = self.find(".ink");
-      //incase of quick double clicks stop the previous animation
-      ink.removeClass("animate");
+  var sqrt2 = Math.sqrt(2);
 
-      //set size of .ink
-      if(!ink.height() && !ink.width())
-      {
-        //use parent's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
-        d = Math.max(self.outerWidth(), self.outerHeight());
-        ink.css({height: d, width: d});
+  function hypot(x, y) { return Math.sqrt((x * x) + (y * y)); }
+
+  $("button").each(function(el) {
+    var self = $(this),
+        html = self.html();
+
+    self.html("").append($('<div class="btn"/>').html(html));
+  })
+  .append($('<div class="ink-visual-container"/>').append($('<div class="ink-visual-static"/>')))
+  
+  .on("select", function(evt) { event.preventDefault(); return false; })
+  .on("mousedown touchstart", function(evt) {
+    event.preventDefault();
+    
+    var self = $(this),
+        container = self.find(".ink-visual-static", true).eq(0);
+
+    if(!container.length) return;
+
+    container.find(".ink-visual").addClass("hide");
+    
+    var rect = this.getBoundingClientRect(),
+        cRect = container[0].getBoundingClientRect(),
+        cx, cy, radius, diam;
+
+        if (event.changedTouches) {
+          cx = event.changedTouches[0].clientX;
+          cy = event.changedTouches[0].clientY;
+        }
+        else {
+          cx = event.clientX;
+          cy = event.clientY;
+        }
+
+    if(self.is(".float")) {
+      var rx = rect.width / 2,
+          ry = rect.height / 2,
+          br = (rx + ry) / 2,
+          mx = rect.left + rx,
+          my = rect.top + ry;
+
+      radius = hypot(cx - mx, cy - my) + br;
+    }
+    else {
+      var w = Math.max(cx - rect.left, rect.right - cx),
+          h = Math.max(cy - rect.top, rect.bottom - cy);
+      
+      radius = hypot(w, h);
+    }
+    diam = radius * 2;
+        
+    var el = $('<div class="ink-visual"/>').width(diam).height(diam)
+    .css("left", cx - cRect.left - radius).css("top", cy - cRect.top - radius)
+    
+    .on("animationend webkitAnimationEnd oanimationend MSAnimationEnd", function() {
+      var self2 = $(this);
+
+      switch(event.animationName) {
+        case "ink-visual-show":
+          self2.addClass("shown");
+          if (!self.is(":active")) self2.addClass("hide");
+          break;
+
+        case "ink-visual-hide":
+          self2.remove();
+          break;
       }
-
-      //get click coordinates
-      //logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
-      x = e.pageX - parent.offset().left - ink.width()/2;
-      y = e.pageY - parent.offset().top - ink.height()/2;
-
-      //set the position and add class .animate
-      ink.css({top: y+'px', left: x+'px'}).addClass("animate");
     })
+    
+    .on("touchend", function() { event.preventDefault(); });
+
+    container.append(el);
+  });
+
+  $(window).on("mouseup touchend", function(evt) {
+    $(".ink-visual-static").children(".ink-visual").addClass("hide");
+  })
+  .on("select selectstart", function(evt) { event.preventDefault(); return false; });
 });
