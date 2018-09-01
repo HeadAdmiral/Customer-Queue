118426e98cc9d548011d5215909887f5885399ee
amastin-microcenter <41840975+amastin-microcenter@users.noreply.github.com>
2018-09-01 01:52:54 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,9 +21,12 @@ function hideModal() {
 }
 
 function addCustomer(){
-  let firstname = document.getElementById("firstname");
-  let lastname = document.getElementById("lastname");
+  let firstname = document.getElementById("fn-input");
+  let lastname = document.getElementById("ln-input");
   let customer = document.createElement("div");
+	
+  console.log(firstname);
+	
   
   hideModal();
   
@@ -33,42 +36,3 @@ function addCustomer(){
   queue.appendChild(customer);
   console.log("added customer to queue");
 }
-
-$(document).ready(function() {
-    $(".btn, #plus").click(function(e) {
-    // Remove olds ones
-    $(".ripple").remove();
-
-    // Setup
-    var posX = $(this).offset().left,
-        posY = $(this).offset().top,
-        buttonWidth = $(this).width(),
-        buttonHeight = $(this).height();
-
-    // Add the element
-    $(this).prepend("<span class='ripple'></span>");
-
-    // Make it round!
-    if (buttonWidth >= buttonHeight) {
-      buttonHeight = buttonWidth;
-    } else {
-      buttonWidth = buttonHeight;
-    }
-
-    // Get the center of the element
-    var x = e.pageX - posX - buttonWidth / 2;
-    var y = e.pageY - posY - buttonHeight / 2;
-      
-    console.log(e.pageX + " - " + posX + " - " + buttonWidth + " / " + 2 + " = " + x);
-    console.log(e.pageY + " - " + posY + " - " + buttonHeight + " / " + 2 + " = " + y);
-	    
-
-    // Add the ripples CSS and start the animation
-    $(".ripple").css({
-      width: buttonWidth,
-      height: buttonHeight,
-      top: y + 'px',
-      left: x + 'px'
-    }).addClass("rippleEffect");
-});
-});
